163dfecc782c17a6650915048e2f03401ccecf3e
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2018-10-26 21:25:34 +0900

    - ABC002問題  - Bクリア

--- ABC002/ABC002_A.cs
+++ ABC002/ABC002_A.cs
@@ -4,13 +4,13 @@ namespace ABC002
 {
     class ABC002_A
     {
-        static void Main(string[] args)
-        {
-            var inputs = Console.ReadLine().Split(' ');
-            int xValue = int.Parse(inputs[0]);
-            int yValue = int.Parse(inputs[1]);
-
-            Console.WriteLine((xValue > yValue ? xValue : yValue));
-        }
+//        static void Main(string[] args)
+//        {
+//            var inputs = Console.ReadLine().Split(' ');
+//            int xValue = int.Parse(inputs[0]);
+//            int yValue = int.Parse(inputs[1]);
+//
+//            Console.WriteLine((xValue > yValue ? xValue : yValue));
+//        }
     }
 }
